f920889a12906100d01025ee2516e33db361d254
saalu <saaluissaka@gmail.com>
2023-07-12 14:35:02 +0000

    Logout and home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import Contact from "./components/Contact";
 import About from "./components/About";
@@ -9,8 +8,11 @@ import Protected from "./components/Protected";
 import axios from "axios";
 import { navigate, Router } from "@reach/router";
 
+export const UserContext = React.createContext([]);
 function App() {
+  const [userInfo, setuserInfo] = useState({});
   const [isLogin, setIsLogin] = useState(false);
+  const [err, setErr] = useState("");
 
   // useEffect(() => {
   //   const checkLogin = async () => {
@@ -27,17 +29,31 @@ function App() {
   //   };
   //   checkLogin();
   // });
+  const logOutCallBack = async () => {
+    try {
+      await axios.get("/api/users/logout");
+
+      localStorage.clear();
+
+      navigate("/");
+    } catch (error) {
+      setErr(error.response.data);
+      console.log(error.message);
+    }
+  };
 
   return (
-    <div className="App">
-      {/* {isLogin ? <Home /> : <Login setIsLogin={setIsLogin} />  } */}
-      <Router id="router">
-        <Login path="login" setIsLogin={setIsLogin} />
-        <Home path="/" />
-        <Register path="register" />
-        <Protected path="protected" />
-      </Router>
-    </div>
+    <UserContext.Provider value={[userInfo, setuserInfo]}>
+      <div className="App">
+        {/* {isLogin ? <Home /> : <Login setIsLogin={setIsLogin} />  } */}
+        <Router id="router">
+          <Login path="login" setIsLogin={setIsLogin} />
+          <Home path="/" logOutCallBack={logOutCallBack} />
+          <Register path="register" />
+          <Protected path="protected" />
+        </Router>
+      </div>
+    </UserContext.Provider>
   );
 }
 
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,24 +1,30 @@
-import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import React, { useEffect } from "react";
 import Login from "./RegisterLogin/login";
 import Register from "./RegisterLogin";
 import Protected from "./Protected";
 import Nav from "./Nav";
 import { Redirect } from "@reach/router";
+import { UserContext } from "../App";
 
-function Home({ setIsLogin }) {
+function Home({ logOutCallBack }) {
   const token = localStorage.getItem("tokenStore");
   if (!token) return <Redirect from="" to="login" noThrow />;
+
   return (
     <div>
-      {/* <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/protected" element={<Protected />} />
-        <Route path="/register" element={<Register />} />
-      </Routes> */}
-
-      <h2>Home Page</h2>
-      <Nav />
+      <Nav logOutCallBack={logOutCallBack} />
+      <div className="container">
+        <div className="row">
+          <div className="col s12 m6">
+            <div className="card blue-grey darken-1">
+              <div className="card-content white-text">
+                <span className="card-title">User Name</span>
+                <p>User Email</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
